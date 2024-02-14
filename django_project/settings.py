e78f2a912313d9b2e1870c25ca193e19a1035433
theloop2 <aguileraa835@gmail.com>
2024-02-14 11:11:30 -0400

    added templates

--- django_project/settings.py
+++ django_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "TEMPLATES"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
